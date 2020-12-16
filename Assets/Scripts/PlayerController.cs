26d238d93d1f509d4d670f577676190c32df9244
LT <support@teran.tech>
2020-12-15 22:34:12 -0700

    Created side scroller game. Player doesn't move, environment moves.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   public bool isOnGround = true;
+   public float jumpForce = 10;
+   public float gravityModifier;
+
+   private Rigidbody playerRb;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      playerRb = GetComponent<Rigidbody>();
+      Physics.gravity *= gravityModifier;
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+      {
+         isOnGround = false;
+         playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+      }
+   }
+
+   void OnCollisionEnter(Collision other) {
+      isOnGround = true;   
+   }
+}
