2d028c784ea15d6de887fe2adb81aa48b2c6142e
LT <support@teran.tech>
2020-12-22 11:45:54 -0700

    Prototype 3    - Completed Lesson 3    - Added music, particle effects, and sounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,16 +8,22 @@ public class PlayerController : MonoBehaviour
    public bool gameOver = false;
    public float jumpForce = 10;
    public float gravityModifier;
+   public AudioClip jumpSound;
+   public AudioClip crashSound;
+   public ParticleSystem explosionParticle;
+   public ParticleSystem dirtParticle;
 
    private Rigidbody playerRb;
    private Animator playerAnim;
+   private AudioSource playerAudio;
 
    // Start is called before the first frame update
    void Start()
    {
       playerRb = GetComponent<Rigidbody>();
       playerAnim = GetComponent<Animator>();
-
+      playerAudio = GetComponent<AudioSource>();
+      
       Physics.gravity *= gravityModifier;
    }
 
@@ -29,6 +35,8 @@ public class PlayerController : MonoBehaviour
          isOnGround = false;
          playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
          playerAnim.SetTrigger("Jump_trig");
+         dirtParticle.Stop();
+         playerAudio.PlayOneShot(jumpSound, 1.0f);
       }
    }
 
@@ -37,13 +45,17 @@ public class PlayerController : MonoBehaviour
       if (other.gameObject.CompareTag("Ground"))
       {
          isOnGround = true;
+         dirtParticle.Play();
       }
       else if (other.gameObject.CompareTag("Obstacle"))
       {
          // Game Over
          gameOver = true;
+         playerAudio.PlayOneShot(crashSound, 1.0f);
          playerAnim.SetBool("Death_b", true);
          playerAnim.SetInteger("DeathType_int", 1);
+         explosionParticle.Play();
+         dirtParticle.Stop();
       }
    }
 }
